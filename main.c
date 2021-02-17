2d764f7567fafdea45b2420ac8042510f3efad3c
Alexander Bessonov <alexander.bessonov548@gmail.com>
2021-02-17 17:46:59 +0700

    Created procedure PrintChessboard.

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+char A[9][9];
+int i,j;
+
+void PrintChessboard(){
+  for (j=8;j>=0;j--){
+    for (i=0;i<=8;i++){
+      printf("%c ",A[i][j]);
+    }
+    printf("\n");
+  }
+}
 
 int main()
 {
+
+return 0;
 }
