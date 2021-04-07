dcea639021abac08f8d20f812d1068feda55b0e8
Alexander Bessonov <alexander.bessonov548@gmail.com>
2021-04-07 18:27:36 +0700

    Added CleanChessboard

--- src/libchessviz/board.h
+++ src/libchessviz/board.h
@@ -3,6 +3,7 @@
 #define BOARD_H
 #include <libchessviz/helper.h>
 
+void CleanChessboard(char Chessboard[9][9]);
 void RefreshChessboard(char Chessboard[9][9]);
 void PrintChessboard(char Chessboard[9][9]);
 
