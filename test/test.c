ec7717b86cafe2b74a3fccfdadcc3da0676eb69f
Alexander Bessonov <alexander.bessonov548@gmail.com>
2021-04-21 18:51:19 +0700

    Added simple test

---
+++ test/test.c
@@ -0,0 +1,22 @@
+#include <libchessviz/board.h>
+#include <libchessviz/helper.h>
+#include <libchessviz/move.h>
+
+#include <ctest.h>
+
+CTEST(pawn, move1)
+{
+	char Chessboard[SIZE_OF_CHESSBOARD][SIZE_OF_CHESSBOARD];
+	Square square1, square2;
+	square1.file = 5;
+	square1.rank = 2;
+	square2.file = 5;
+	square2.rank = 4;
+   	CleanChessboard(Chessboard);
+    	ChessBoard[square1.file][square1.rank] = 'p';
+    
+    	const int result = DoMove(Chessboard,square1,square2,0,0,0,0);
+
+   	const int expected = 0;
+   	ASSERT_EQUAL(expected, result);
+}
