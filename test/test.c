be9cb5c809d0912b41cc2340e579d61681bb7f66
bessonov <box@email.ru>
2021-04-26 17:45:20 +0700

    Removed stdio

--- test/test.c
+++ test/test.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-
 #include <libchessviz/board.h>
 #include <libchessviz/helper.h>
 #include <libchessviz/move.h>
@@ -8,18 +6,17 @@
 
 CTEST(pawn, move1)
 {
-	char Chessboard[SIZE_OF_CHESSBOARD][SIZE_OF_CHESSBOARD];
-	Square square1, square2;
-	square1.file = 5;
-	square1.rank = 2;
-	square2.file = 5;
-	square2.rank = 4;
-   	CleanChessboard(Chessboard);
-    	Chessboard[square1.file][square1.rank] = 'P';
-	printf("\n");
-    
-    	const int result = DoMove(Chessboard,square1,square2,0,0,0,0);
+    char Chessboard[SIZE_OF_CHESSBOARD][SIZE_OF_CHESSBOARD];
+    Square square1, square2;
+    square1.file = 5;
+    square1.rank = 2;
+    square2.file = 5;
+    square2.rank = 4;
+    CleanChessboard(Chessboard);
+    Chessboard[square1.file][square1.rank] = 'P';
 
-   	const int expected = 0;
-   	ASSERT_EQUAL(expected, result);
+    const int result = DoMove(Chessboard, square1, square2, 0, 0, 0, 0);
+
+    const int expected = 0;
+    ASSERT_EQUAL(expected, result);
 }
