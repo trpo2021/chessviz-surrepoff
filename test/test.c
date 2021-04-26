0d394da531772557962e6ad90683211fa179ea45
bessonov <box@email.ru>
2021-04-26 20:37:42 +0700

    Added group of pawn tests

--- test/test.c
+++ test/test.c
@@ -4,7 +4,31 @@
 
 #include <ctest.h>
 
-CTEST(pawn, move1)
+CTEST(pawn, movewhite1)
+{
+    char Chessboard[SIZE_OF_CHESSBOARD][SIZE_OF_CHESSBOARD];
+    int piece, turn, movetype, moveend;
+    Square square1, square2;
+    square1.file = 5;
+    square1.rank = 2;
+    square2.file = 5;
+    square2.rank = 3;
+    piece = 0;
+    turn = 0;
+    movetype = 0;
+    moveend = 0;
+
+    CleanChessboard(Chessboard);
+    Chessboard[square1.file][square1.rank] = 'P';
+
+    const int result = DoMove(
+            Chessboard, square1, square2, piece, turn, movetype, moveend);
+
+    const int expected = 0;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(pawn, movewhite2)
 {
     char Chessboard[SIZE_OF_CHESSBOARD][SIZE_OF_CHESSBOARD];
     int piece, turn, movetype, moveend;
@@ -20,6 +44,154 @@ CTEST(pawn, move1)
 
     CleanChessboard(Chessboard);
     Chessboard[square1.file][square1.rank] = 'P';
+
+    const int result = DoMove(
+            Chessboard, square1, square2, piece, turn, movetype, moveend);
+
+    const int expected = 0;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(pawn, moveblack1)
+{
+    char Chessboard[SIZE_OF_CHESSBOARD][SIZE_OF_CHESSBOARD];
+    int piece, turn, movetype, moveend;
+    Square square1, square2;
+    square1.file = 5;
+    square1.rank = 7;
+    square2.file = 5;
+    square2.rank = 6;
+    piece = 0;
+    turn = 1;
+    movetype = 0;
+    moveend = 0;
+
+    CleanChessboard(Chessboard);
+    Chessboard[square1.file][square1.rank] = 'p';
+
+    const int result = DoMove(
+            Chessboard, square1, square2, piece, turn, movetype, moveend);
+
+    const int expected = 0;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(pawn, moveblack2)
+{
+    char Chessboard[SIZE_OF_CHESSBOARD][SIZE_OF_CHESSBOARD];
+    int piece, turn, movetype, moveend;
+    Square square1, square2;
+    square1.file = 5;
+    square1.rank = 7;
+    square2.file = 5;
+    square2.rank = 5;
+    piece = 0;
+    turn = 1;
+    movetype = 0;
+    moveend = 0;
+
+    CleanChessboard(Chessboard);
+    Chessboard[square1.file][square1.rank] = 'p';
+
+    const int result = DoMove(
+            Chessboard, square1, square2, piece, turn, movetype, moveend);
+
+    const int expected = 0;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(pawn, movewhitecaptureL)
+{
+    char Chessboard[SIZE_OF_CHESSBOARD][SIZE_OF_CHESSBOARD];
+    int piece, turn, movetype, moveend;
+    Square square1, square2;
+    square1.file = 5;
+    square1.rank = 2;
+    square2.file = 4;
+    square2.rank = 3;
+    piece = 0;
+    turn = 0;
+    movetype = 1;
+    moveend = 0;
+
+    CleanChessboard(Chessboard);
+    Chessboard[square1.file][square1.rank] = 'P';
+    Chessboard[square2.file][square2.rank] = 'p';
+
+    const int result = DoMove(
+            Chessboard, square1, square2, piece, turn, movetype, moveend);
+
+    const int expected = 0;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(pawn, movewhitecaptureR)
+{
+    char Chessboard[SIZE_OF_CHESSBOARD][SIZE_OF_CHESSBOARD];
+    int piece, turn, movetype, moveend;
+    Square square1, square2;
+    square1.file = 5;
+    square1.rank = 2;
+    square2.file = 6;
+    square2.rank = 3;
+    piece = 0;
+    turn = 0;
+    movetype = 1;
+    moveend = 0;
+
+    CleanChessboard(Chessboard);
+    Chessboard[square1.file][square1.rank] = 'P';
+    Chessboard[square2.file][square2.rank] = 'p';
+
+    const int result = DoMove(
+            Chessboard, square1, square2, piece, turn, movetype, moveend);
+
+    const int expected = 0;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(pawn, moveblackcaptureL)
+{
+    char Chessboard[SIZE_OF_CHESSBOARD][SIZE_OF_CHESSBOARD];
+    int piece, turn, movetype, moveend;
+    Square square1, square2;
+    square1.file = 5;
+    square1.rank = 7;
+    square2.file = 4;
+    square2.rank = 6;
+    piece = 0;
+    turn = 1;
+    movetype = 1;
+    moveend = 0;
+
+    CleanChessboard(Chessboard);
+    Chessboard[square1.file][square1.rank] = 'p';
+    Chessboard[square2.file][square2.rank] = 'P';
+
+    const int result = DoMove(
+            Chessboard, square1, square2, piece, turn, movetype, moveend);
+
+    const int expected = 0;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(pawn, moveblackcaptureR)
+{
+    char Chessboard[SIZE_OF_CHESSBOARD][SIZE_OF_CHESSBOARD];
+    int piece, turn, movetype, moveend;
+    Square square1, square2;
+    square1.file = 5;
+    square1.rank = 7;
+    square2.file = 6;
+    square2.rank = 6;
+    piece = 0;
+    turn = 1;
+    movetype = 1;
+    moveend = 0;
+
+    CleanChessboard(Chessboard);
+    Chessboard[square1.file][square1.rank] = 'p';
+    Chessboard[square2.file][square2.rank] = 'P';
 
     const int result = DoMove(
             Chessboard, square1, square2, piece, turn, movetype, moveend);
